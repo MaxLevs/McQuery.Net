2eea0ee497714e527ee9d6013fcc1a4294497acf
Liven Maxim <maxlevs@mail.ru>
2024-11-17 00:02:33 +0300

    feat: implement parsing of full status response

--- sources/McQuery.Net/Data/Parsers/FullStatusResponseParser.cs
+++ sources/McQuery.Net/Data/Parsers/FullStatusResponseParser.cs
@@ -9,10 +9,50 @@ internal class FullStatusResponseParser : StatusResponseParser<FullStatus>
 
     private static readonly byte[] Constant2 = [0x01, 0x70, 0x6C, 0x61, 0x79, 0x65, 0x72, 0x5F, 0x00, 0x00];
 
+    private static readonly InvalidOperationException ResponseFormatError = new("Invalid full status response format");
+
     public override FullStatus Parse(byte[] data)
     {
         SessionId sessionId = StartParsing(data, out SequenceReader<byte> reader);
 
-        throw new NotImplementedException();
+        if (!reader.IsNext(Constant1, true))
+        {
+            throw ResponseFormatError;
+        }
+
+        Dictionary<string, string> statusKeyValues = new();
+        while (!reader.IsNext(0, true))
+        {
+            string key = ParseNullTerminatingString(ref reader);
+            string value = ParseNullTerminatingString(ref reader);
+            statusKeyValues.Add(key, value);
+        }
+
+        if (!reader.IsNext(Constant2, true))
+        {
+            throw ResponseFormatError;
+        }
+
+        List<string> players = [];
+        while (!reader.IsNext(0, true))
+        {
+            players.Add(ParseNullTerminatingString(ref reader));
+        }
+
+        return new FullStatus(
+            statusKeyValues["hostname"],
+            statusKeyValues["gametype"],
+            statusKeyValues["game_id"],
+            statusKeyValues["version"],
+            statusKeyValues["plugins"],
+            statusKeyValues["map"],
+            int.Parse(statusKeyValues["numplayers"]),
+            int.Parse(statusKeyValues["maxplayers"]),
+            players.ToArray(),
+            int.Parse(statusKeyValues["hostport"]),
+            statusKeyValues["hostip"])
+        {
+            SessionId = sessionId,
+        };
     }
 }
