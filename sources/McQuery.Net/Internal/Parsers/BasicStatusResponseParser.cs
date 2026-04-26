1013024cfc830b9f01b12a0e07c07ac06f4cf0f2
Maxim Liven <maxlevs@mail.ru>
2026-04-26 09:38:31 +0300

    feat: basic format exception

--- sources/McQuery.Net/Internal/Parsers/BasicStatusResponseParser.cs
+++ sources/McQuery.Net/Internal/Parsers/BasicStatusResponseParser.cs
@@ -1,4 +1,5 @@
 using McQuery.Net.Data;
+using McQuery.Net.Exceptions;
 
 namespace McQuery.Net.Internal.Parsers;
 
@@ -6,19 +7,35 @@ internal class BasicStatusResponseParser : StatusResponseParser<BasicStatus>
 {
     public override BasicStatus Parse(byte[] data)
     {
-        var sessionId = StartParsing(data, out var reader);
-
-        return new BasicStatus(
-            ParseNullTerminatingString(ref reader),
-            ParseNullTerminatingString(ref reader),
-            ParseNullTerminatingString(ref reader),
-            int.Parse(ParseNullTerminatingString(ref reader)),
-            int.Parse(ParseNullTerminatingString(ref reader)),
-            ParseShortLittleEndian(ref reader),
-            ParseNullTerminatingString(ref reader)
-        )
+        try
         {
-            SessionId = sessionId,
-        };
+
+            var sessionId = StartParsing(data, out var reader);
+
+            var motd = ParseNullTerminatingString(ref reader);
+            var gameType = ParseNullTerminatingString(ref reader);
+            var map = ParseNullTerminatingString(ref reader);
+            var numPlayers = int.Parse(ParseNullTerminatingString(ref reader));
+            var maxPlayers = int.Parse(ParseNullTerminatingString(ref reader));
+            var hostPort = ParseShortLittleEndian(ref reader);
+            var hostIp = ParseNullTerminatingString(ref reader);
+
+            return new BasicStatus(
+                motd,
+                gameType,
+                map,
+                numPlayers,
+                maxPlayers,
+                hostPort,
+                hostIp)
+            {
+                SessionId = sessionId,
+            };
+        }
+
+        catch (Exception exception)
+        {
+            throw new McQueryException("Invalid basic status response format", exception);
+        }
     }
 }
